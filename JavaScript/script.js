cdbce8269049bf3133b383554fb483f884625dfa
GuiVideira <guilhermevideira@outlook.com.br>
2023-05-26 08:50:10 -0300

    string, numbers, booleans

---
+++ JavaScript/script.js
@@ -0,0 +1,13 @@
+//números
+console.log(2+2)
+console.log(2/2)
+console.log(2*3)
+
+//String
+console.log('mega Perlon')
+console.log('Eu serei um excelente Dev FrontEnd, confia!')
+console.log(`Vem com nois ${222+555}`)
+
+//Boolean
+console.log(true)
+console.log(false)
