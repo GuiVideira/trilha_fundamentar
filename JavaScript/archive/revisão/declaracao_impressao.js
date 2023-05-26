9a9a5837d516f1b04f7362bfe85fe5b75c164415
GuiVideira <guilhermevideira@outlook.com.br>
2023-05-26 09:51:45 -0300

    Studying

---
+++ JavaScript/archive/revisão/declaracao_impressao.js
@@ -0,0 +1,21 @@
+
+//Variaveis e tipos de dados
+//Declaração or declaration
+var nickname
+
+//assignment or atribuição de valores
+nickname = "perlon"
+
+// q tipo de dado foi colocado na variavel
+console.log(nickname)
+
+//numbers/ agrupamento de de declarações
+let age, isHuman 
+age = 20
+isHuman = true
+
+//imprimindo multiplos 
+console.log(nickname, age, isHuman)
+
+//escrita de texto + variaveis(concatenação e interpolação)
+console.log(`O ${nickname} tem ${age} anos.`)
