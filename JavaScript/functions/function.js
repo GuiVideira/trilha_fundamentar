4f79694d51982bb54a9d9b45924cb1c54a525b82
GuiVideira <guilhermevideira@outlook.com.br>
2023-05-26 11:53:19 -0300

    learning functions

---
+++ JavaScript/functions/function.js
@@ -0,0 +1,15 @@
+//criar um app de frases
+
+console.log("Vamos Apreonder porgramação");
+console.log("Os estudos compemsam");
+console.log("Feliz é que sabe programar");
+
+//declaration function statement
+function createPhrases() {
+  console.log("Vamos Apreonder porgramação")
+  console.log("Os estudos compemsam")
+  console.log("Feliz é que sabe programar")
+}
+
+//execute 
+createPhrases()
