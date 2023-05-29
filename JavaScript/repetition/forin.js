c10333111198736c0d73362152271be8a7b7c114
GuiVideira <guilhermevideira@outlook.com.br>
2023-05-29 20:45:27 -0300

    repetition

---
+++ JavaScript/repetition/forin.js
@@ -0,0 +1,14 @@
+// Estruturas de repetição 
+//For in
+
+let person ={
+    nickname: 'Perlon',
+    age: 20,
+    weght: 73.5
+}
+
+for (let property in person) {
+    console.log(property)
+    console.log(person[property])
+
+}
