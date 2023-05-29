9e4254ae81554efe30260d0118df3d3dc7470fd0
GuiVideira <guilhermevideira@outlook.com.br>
2023-05-29 07:00:10 -0300

    operators

---
+++ JavaScript/expressions/operadores.js
@@ -0,0 +1,33 @@
+/*operadores aritméticos,
+ que nos possibilitam fazer
+  cálculos matemáticos.*/
+    
+//multiplicação
+console.log("multiplicação:",3.3*6)
+
+//divisão
+console.log("divisão:",12/4)
+
+//soma
+console.log("soma:",15+04)
+
+//subtração
+console.log("subtração:",31-04)
+
+//resto da divisão
+let remainder
+remainder = 11% 3
+console.log("resto da divisão:",remainder)
+
+//increcimo 
+let increment = 0
+increment++
+console.log("incrememento:",increment)
+
+//decrecimo
+let decrecimo = 3
+decrecimo--
+console.log("decremeimo:",decrecimo)
+
+//exponencial
+console.log("exponencial:",3**3)
---
+++ JavaScript/expressions/operadorescomparação.js
@@ -0,0 +1,16 @@
+/*Comparação iual e deiferente.*/
+    
+let one = 1;
+let two = 2;
+
+//comparação
+console.log( two == 1)
+console.log( one =="1")
+
+//estritamente igal a 
+console.log( one === "1") //aqui o um é string
+console.log( one === 1) // aqui o um é numero mesmo
+
+//estritamente difernte de 
+console.log(two !== "2")
+console.log(two !== 2)
