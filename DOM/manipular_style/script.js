15f71717a1c5aa5937392b83a3d60f6320a61958
GuiVideira <guilhermevideira@outlook.com.br>
2023-06-06 09:08:14 -0300

    file org

---
+++ DOM/manipular_style/script.js
@@ -0,0 +1,15 @@
+//Alterando Estilos
+//const element = document.querySelector('body')
+
+//element.style.backgroundColor="#f9f3d2"
+//console.log(element.style.backgroundColor)
+//console.log(element.style.backgroundColor)
+
+
+//classList
+const element = document.querySelector('body')
+
+element.classList.add('active', 'green')//poem estilo
+console.log(element.classList)
+element.classList.remove('active')//retira estilo
+element.classList.toggle('active')//se tem tira, se n tem, poem
