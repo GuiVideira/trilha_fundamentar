c23453d0a89a45ecf5623ebd11d07dac18e36bce
GuiVideira <guilhermevideira@outlook.com.br>
2023-06-16 09:01:47 -0300

    new process

--- node/process.js
+++ node/process.js
@@ -1,6 +1,8 @@
 //lista de argumentos 
 
-const firstName = process.argv[2]
-const lastName = process.argv[3]
+console.log(process.argv)
 
-console.log(`Seu nom é ${firstName} ${lastName}`)
+//const firstName = process.argv[2]
+//const lastName = process.argv[3]
+
+//console.log(`Seu nom é ${firstName} ${lastName}`)
