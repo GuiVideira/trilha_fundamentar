d10bf3855b7cc64dd8e3eab0e32556905c56ce93
GuiVideira <guilhermevideira@outlook.com.br>
2023-06-20 10:10:26 -0300

    new fucntion

--- BestMe/index.js
+++ BestMe/index.js
@@ -1,13 +1,13 @@
 const questions = [
     "O que aprendi hoje?",
-    "I que me deixou aborrecido?",
+    "O que me deixou aborrecido?",
     "O que eu poderia fazer para melhorar ?",
     "O que me deixou feliz hoje?",
     "Quantas pessoas eu ajudei hoje?",
 ]
 
 const ask = (index = 0 ) =>{
-    process.stdout.write(questions[index] + "\n\n\n")
+    process.stdout.write("\n\n" + questions[index] + " > ")
 }
 
 ask()
@@ -17,6 +17,27 @@ process.stdin.on("data", data => {
     answers.push(data.toString().trim())
     if (answers.length < questions.length){
         ask(answers.length)
+    } else {
+        console.log(answers)
+        process.exit()
     }
-    process.exit()
+})
+
+process.on('exit', ()=> {
+    console.log(`
+    Legal Perlon!
+
+    O que voce aprendeu foi:
+    ${answers[0]}
+
+    O que te aborreceu foi:
+    ${answers[1]}
+
+    O que te deizou feliz hoje:
+    ${answers[2]}
+
+    Voce ajudou ${answers[3]} pessoas hoje!
+
+    Volte amanha!
+    `)
 })
