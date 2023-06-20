be11b38f1fed54741c8a88a73bec464e2eab2514
GuiVideira <guilhermevideira@outlook.com.br>
2023-06-20 09:34:29 -0300

    questions

---
+++ BestMe/index.js
@@ -0,0 +1,7 @@
+const questions = [
+    "O que aprendi hoje?",
+    "I que me deixou aborrecido?",
+    "O que eu poderia fazer para melhorar ?",
+    "O que me deixou feliz hoje?",
+    "Quantas pessoas eu ajudei hoje?",
+]
